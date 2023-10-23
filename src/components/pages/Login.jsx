22eae80ed2385b3a62dc670ae3f4ab77889c2964
Kudikala Usha Rani <143144092+usha8719@users.noreply.github.com>
2023-10-23 04:20:50 -0400

    Update Login.jsx

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../../../src/components/login.css';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { Link } from 'react-router-dom';
 
 
 function Login() {
